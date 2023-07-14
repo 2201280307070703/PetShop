c9d7aac530ed42213e58b09ec133b55af19a40c4
Yanita Kyuchukova <111355885+2201280307070703@users.noreply.github.com>
2023-07-14 13:55:05 +0300

    Adding BecomeSellerFormModel

---
+++ PetShop.Web.ViewModels/Seller/BecomeSellerFormModel.cs
@@ -0,0 +1,29 @@
+﻿namespace PetShop.Web.ViewModels.Seller
+{
+    using System.ComponentModel.DataAnnotations;
+    using static PetShop.Common.EntityValidationConstants.Seller;
+    public class BecomeSellerFormModel
+    {
+        [Required]
+        [StringLength(FirstNameMaxLength, MinimumLength=FirstNameMinLength)]
+        [Display(Name ="First name")]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [StringLength(LastNameMaxLength, MinimumLength =LastNameMinLength)]
+        [Display(Name ="Last name")]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [StringLength(PhoneNumberMaxLength, MinimumLength=PhoneNumberMinLength)]
+        [Phone]
+        [Display(Name ="Phone")]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required]
+        [StringLength(EmailMaxLength,MinimumLength =EmailMinLength)]
+        [EmailAddress]
+        [Display(Name ="Email")]
+        public string Email { get; set; } = null!;
+    }
+}
