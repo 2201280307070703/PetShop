08208c407e48a6061839e03f25ee1b4020a2d544
Yanita Kyuchukova <111355885+2201280307070703@users.noreply.github.com>
2023-07-18 13:43:50 +0300

    Add custom model binder

---
+++ PetShop.Web.Infrastructure/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,47 @@
+﻿namespace PetShop.Web.Infrastructure.ModelBinders
+{
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using System.Globalization;
+
+    internal class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext? bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            ValueProviderResult valueResult =
+                bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if(valueResult!=ValueProviderResult.None && !string.IsNullOrWhiteSpace(valueResult.FirstValue))
+            {
+                decimal parsedValue = 0m;
+                bool binderSucceed = false;
+
+                try
+                {
+                    string formDecimalValue = valueResult.FirstValue;
+
+                    formDecimalValue = formDecimalValue.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                    formDecimalValue = formDecimalValue.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    parsedValue=Convert.ToDecimal(formDecimalValue);
+                    binderSucceed = true;
+                }
+                catch(FormatException fe)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+                }
+
+                if (binderSucceed)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(parsedValue);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
