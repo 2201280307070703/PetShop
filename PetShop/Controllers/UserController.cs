74601f26b40f8d4a3e9651e097baa63ba1552a7b
Yanita Kyuchukova <111355885+2201280307070703@users.noreply.github.com>
2023-07-25 18:19:23 +0300

    Implementing login functionality.

--- PetShop/Controllers/UserController.cs
+++ PetShop/Controllers/UserController.cs
@@ -1,9 +1,11 @@
 ﻿namespace PetShop.Controllers
 {
+    using Microsoft.AspNetCore.Authentication;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using PetShop.Data.Models;
     using PetShop.Web.ViewModels.User;
+    using static PetShop.Common.NotificationMessagesConstants;
 
     public class UserController : Controller
     {
@@ -51,6 +53,39 @@
 
             await signInManager.SignInAsync(user, false);
             return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(string? returnUrl = null)
+        {
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            LoginFormModel model=new LoginFormModel()
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result =
+                await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+            if (!result.Succeeded)
+            {
+                this.TempData[ErrorMessage] = "Something went wrong while logging you! Please try again.";
+                return View(model);
+            }
+
+            return Redirect(model.ReturnUrl ?? "/Home/Index");
         }
     }
 }
