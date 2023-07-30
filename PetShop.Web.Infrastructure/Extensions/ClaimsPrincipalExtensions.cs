2a1e5720bd73252dd846cc83ace26dd9aea367c0
Yanita Kyuchukova <111355885+2201280307070703@users.noreply.github.com>
2023-07-30 18:03:45 +0300

    Adding claims principal extension for checking for role

--- PetShop.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
+++ PetShop.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,11 +1,17 @@
 ﻿namespace PetShop.Web.Infrastructure.Extensions
 {
     using System.Security.Claims;
+    using static PetShop.Common.GeneralApplicationConstants;
     public static  class ClaimsPrincipalExtensions
     {
         public static string? GetId(this ClaimsPrincipal user)
         {
             return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        public static bool IsUserAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRoleName);
         }
     }
 }
