b8c2d0033313d9d34efd42549956882969902d13
Yanita Kyuchukova <111355885+2201280307070703@users.noreply.github.com>
2023-08-11 18:41:47 +0300

    add tests

---
+++ PetShop.Services.Tests/SellerServiceTests.cs
@@ -0,0 +1,16 @@
+namespace PetShop.Services.Tests
+{
+    public class SellerServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
